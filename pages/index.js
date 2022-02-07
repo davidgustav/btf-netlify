0d021c46d6578946c2fdddf623ccc10c69cdbd47
Dave Anderson <dave@karmamethod.com>
2022-02-07 12:58:42 -0500

    Initial changes to the page

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Bump This Fist</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/cropped-btf-site-icon-32.png" sizes="32x32"></link>
+        <link rel="icon" href="/cropped-btf-site-icon-192.png" sizes="192x192"></link>
+        <link rel="apple-touch-icon" href="/cropped-btf-site-icon-180.png"></link>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bump This Fist" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Thanks for visiting. We've pivoted to a consultancy for now and will be removing the store until further notice.
         </p>
       </main>
 
